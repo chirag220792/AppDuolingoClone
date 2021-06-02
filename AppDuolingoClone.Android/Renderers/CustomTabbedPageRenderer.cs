6979b08b54f236097a6f5d103ff206380bbf7345
Chirag <Chirag@HO-CHIRAG>
2021-06-02 18:04:36 +0530

    Fixed TabMenu

---
+++ AppDuolingoClone.Android/Renderers/CustomTabbedPageRenderer.cs
@@ -0,0 +1,92 @@
+﻿using Android.Content;
+using Android.Support.Design.Widget;
+using AppDuolingoClone.Droid.Renderers;
+using AppDuolingoClone.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms.Platform.Android.AppCompat;
+
+[assembly: ExportRenderer(typeof(TabbedPage), typeof(CustomTabbedPageRenderer))]
+namespace AppDuolingoClone.Droid.Renderers
+{
+    public class CustomTabbedPageRenderer : TabbedPageRenderer
+    {
+
+        private TabbedPage _formsTabs;
+        private BottomNavigationView _bottomNavigationView;
+
+        public CustomTabbedPageRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<TabbedPage> e)
+        {
+            base.OnElementChanged(e);
+            if(e.NewElement != null)
+            {
+                _formsTabs = Element;
+                _formsTabs.CurrentPageChanged += OnCurrentPageChanged;
+
+                var relativeLayout = base.GetChildAt(0) as Android.Widget.RelativeLayout;
+                _bottomNavigationView = relativeLayout.GetChildAt(1) as BottomNavigationView;
+                _bottomNavigationView.SetMinimumHeight(300);
+                _bottomNavigationView.ItemIconTintList = null;
+                _bottomNavigationView.ItemIconSize = 150;
+                UpdateAllTabs(_formsTabs, _bottomNavigationView);
+            }
+
+        }
+        private void UpdateAllTabs(TabbedPage formsTabs, BottomNavigationView bottomNavigationView)
+        {
+            for (var index = 0; index < formsTabs.Children.Count; index++)
+            {
+                var androidTab = bottomNavigationView.Menu.GetItem(index);
+                if (formsTabs.Children[index] is LessonsView)
+                {
+                    if (formsTabs.Children[index] == formsTabs.CurrentPage)
+                    {
+                        androidTab.SetIcon(Resource.Drawable.tab_lessons_selected);
+                        continue;
+                    }
+                    androidTab.SetIcon(Resource.Drawable.tab_lessons);
+                    continue;
+                }
+                if (formsTabs.Children[index] is ProfileView)
+                {
+                    if (formsTabs.Children[index] == formsTabs.CurrentPage)
+                    {
+                        androidTab.SetIcon(Resource.Drawable.tab_profile_selected);
+                        continue;
+                    }
+                    androidTab.SetIcon(Resource.Drawable.tab_profile);
+                    continue;
+                }
+                if (formsTabs.Children[index] is RankingView)
+                {
+                    if (formsTabs.Children[index] == formsTabs.CurrentPage)
+                    {
+                        androidTab.SetIcon(Resource.Drawable.tab_ranking_selected);
+                        continue;
+                    }
+                    androidTab.SetIcon(Resource.Drawable.tab_ranking);
+                    continue;
+                }
+                if (formsTabs.Children[index] is StoreView)
+                {
+                    if (formsTabs.Children[index] == formsTabs.CurrentPage)
+                    {
+                        androidTab.SetIcon(Resource.Drawable.tab_store_selected);
+                        continue;
+                    }
+                    androidTab.SetIcon(Resource.Drawable.tab_store);
+                    continue;
+                }
+            }
+        }
+        private void OnCurrentPageChanged(object sender, System.EventArgs e)
+        {
+            UpdateAllTabs(_formsTabs, _bottomNavigationView);
+            //System.Diagnostics.Debug.WriteLine("");
+        }
+    }
+}
--- AppDuolingoClone.iOS/Renderers/CustomTabbedPageRenderer.cs
+++ AppDuolingoClone.iOS/Renderers/CustomTabbedPageRenderer.cs
@@ -1,9 +1,11 @@
 ﻿using AppDuolingoClone.iOS.Renderers;
+using AppDuolingoClone.Views;
 using Foundation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using UIKit;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
@@ -13,9 +15,73 @@ namespace AppDuolingoClone.iOS.Renderers
 {
     public class CustomTabbedPageRenderer : TabbedRenderer
     {
-        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        public override void ViewDidLayoutSubviews()
         {
-            base.OnElementChanged(e);
+            base.ViewDidLayoutSubviews();
+
+            var tabFrame = TabBar.Frame;
+            var tabHeight = 150;
+            //var viewFrame = View.Frame;
+
+            tabFrame.Height = tabHeight;
+            tabFrame.Y = View.Frame.Height - tabHeight;
+            //viewFrame.Y = View.Frame.Height - 150;
+            //viewFrame.Height = View.Frame.Height - 150;
+
+            TabBar.Frame = tabFrame;
+            //View.Frame = viewFrame;
+        }
+
+        protected override async Task<Tuple<UIImage, UIImage>> GetIcon(Page page)
+        {
+            if (page is LessonsView)
+                return await Task.FromResult(
+                    new Tuple<UIImage, UIImage>(
+                        GetImageFromFile("tab_lessons.png"),
+                        GetImageFromFile("tab_lessons_selected.png")
+                     )
+                );
+
+            if (page is TrainingView)
+                return await Task.FromResult(
+                    new Tuple<UIImage, UIImage>(
+                        GetImageFromFile("tab_training.png"),
+                        GetImageFromFile("tab_training_selected.png")
+                     )
+                );
+
+            if (page is ProfileView)
+                return await Task.FromResult(
+                    new Tuple<UIImage, UIImage>(
+                        GetImageFromFile("tab_profile.png"),
+                        GetImageFromFile("tab_profile_selected.png")
+                     )
+                );
+
+            if (page is RankingView)
+                return await Task.FromResult(
+                    new Tuple<UIImage, UIImage>(
+                        GetImageFromFile("tab_ranking.png"),
+                        GetImageFromFile("tab_ranking_selected.png")
+                     )
+                );
+
+            if (page is StoreView)
+                return await Task.FromResult(
+                    new Tuple<UIImage, UIImage>(
+                        GetImageFromFile("tab_store.png"),
+                        GetImageFromFile("tab_store_selected.png")
+                     )
+                );
+
+            return await base.GetIcon(page);
+        }
+
+        private UIImage GetImageFromFile(string fileName)
+        {
+            return UIImage
+                .FromFile(fileName)
+                .ImageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal)
         }
     }
 }
